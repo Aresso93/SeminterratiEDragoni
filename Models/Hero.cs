5dd6fa6370848f01a0521d35d11a76d375a72ecd
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2024-01-30 12:26:10 +0100

    Initial commit

---
+++ Models/Hero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeminterratiEDragoni.Models
+{
+    class Hero
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SeminterratiEDragoni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
